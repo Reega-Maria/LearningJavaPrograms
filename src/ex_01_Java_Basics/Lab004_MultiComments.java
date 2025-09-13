6e96a5ea932f2be96baeeb0bff51331a5191f498
Reega-Maria <mariareega4@gmail.com>
2025-09-13 22:18:26 +0530

    Multi-line comments

---
+++ src/ex_01_Java_Basics/Lab004_MultiComments.java
@@ -0,0 +1,22 @@
+package ex_01_Java_Basics;
+
+public class Lab004_MultiComments {
+    /**
+     * Author : Reega Maria
+     * Learning :  Java
+     **/
+        public static void main(String[] args) {
+            /**
+             * Author : Reega Maria
+             * Learning :  Java
+             **/
+       /*
+        System.out.println("Heloo");
+        System.out.println("Heloo");
+        */
+        }
+    }
+/**
+ * Author : Reega
+ * Learning :  Java
+ **/
