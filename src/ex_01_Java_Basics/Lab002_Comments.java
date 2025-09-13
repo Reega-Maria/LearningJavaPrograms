688bb7de896b438e2e9ea7cd3cf896c88bf7d1dc
Reega-Maria <mariareega4@gmail.com>
2025-09-13 19:44:36 +0530

    Comments

---
+++ src/ex_01_Java_Basics/Lab002_Comments.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basics;
+
+public class Lab002_Comments {
+    public static void main(String[] args) {
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        // This is also a comment
+        System.out.println("Hello World!");
+    }
+}
+
+
+
