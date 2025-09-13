cdb0dcd9dd1368f459ffe2d63b8bda1d9ca69d96
Reega-Maria <mariareega4@gmail.com>
2025-09-13 19:54:26 +0530

    Comments

---
+++ src/ex_01_Java_Basics/Lab003_Comments.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab003_Comments {
+    public static void main(String[] args) {
+    }
+    // This is comment, This code will be ignored by the compiler
+    // 2+2
+}
+
