089393169766fcabb08b07f5595013a988ddc554
Reega-Maria <mariareega4@gmail.com>
2025-09-19 12:49:45 +0530

    Interview QnA

---
+++ src/ex_01_Java_Basics/Lab019_Interview_QnA.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab019_Interview_QnA {
+        public static void main(String[] args) {
+            int _2 = 123;
+//_ alone is a reserved keyword. But _2 is still fine.
+            System.out.println(_2);//cannot write int 2_ = 123; or int 2 = 123; because identifiers cannot begin with a digit.
+
+        }
+    }
+
+
---
+++ src/ex_01_Java_Basics/Lab020_Interview_QnA.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basics;
+
+public class Lab020_Interview_QnA {
+        public static void main(String[] args) {
+//        int enum = 10; enum is a keyword in Java (since Java 5).cannot use keywords as identifiers.
+            int Enum = 10;
+//        System.out.println(enum);
+            //        int pramod dutta  = 89; Invalid because spaces are not allowed in identifiers.
+            int pramod$dutta = 90;
+            int Lab020_Interview_QnA2 = 123;
+
+        }
+    }
+
