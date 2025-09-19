c87b89198f6033d40186e6199a22dc5c3cf11696
Reega-Maria <mariareega4@gmail.com>
2025-09-19 12:53:19 +0530

    Boolean Data type

---
+++ src/ex_01_Java_Basics/Lab021_Boolean.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab021_Boolean {
+        public static void main(String[] args) {
+            boolean is_Ammu_married = true;
+            boolean Ammu_has_audi = false;
+            boolean this_is_a_long_name_that_student_asked_from_us_that_is_it_possible = true;
+
+        }
+    }
+
+
