7599602041eeaf05cc247505feb26eed347d90d6
Reega-Maria <mariareega4@gmail.com>
2025-09-19 12:39:58 +0530

    Println Vs Print

---
+++ src/ex_01_Java_Basics/Lab012_Println_Vs_Print.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basics;
+
+public class Lab012_Println_Vs_Print {
+        public static void main(String[] args) {
+       /* System.out.println("Hi");
+        System.out.println("Bye");*/  // ln -> newline
+
+            System.out.print("Hi ");
+            System.out.print("Bye");
+        }
+    }
+
