f99a826060e4e0edf8b5d58013aa647c2b1a5a0d
Reega-Maria <mariareega4@gmail.com>
2025-09-19 12:48:36 +0530

    Identifiers

---
+++ src/ex_01_Java_Basics/Lab018_Identifiers.java
@@ -0,0 +1,15 @@
+package ex_01_Java_Basics;
+
+public class Lab018_Identifiers {
+    public static void main(String[] args) {
+        int _ = 65;// Java 9 and later,a single underscore-reserved keyword no longer used as variable name
+        //int _value =65;
+        //int __ = 65;
+        int $ = 65;
+        int year = 2025;
+        int this_is_a_vari_long_name_in_the_class_hello_$_akakak = 89;
+
+    }
+}
+
+
