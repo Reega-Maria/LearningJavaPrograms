1dcdb97d9765fb94d30cdef7e9b39ce6cc542f64
Reega-Maria <mariareega4@gmail.com>
2025-09-19 11:31:12 +0530

    Keywords and Identifiers

---
+++ src/ex_01_Java_Basics/Lab008_Keywords_Identifiers.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basics;
+
+public class Lab008_Keywords_Identifiers {
+        public static void main(String[] args) {
+            System.out.println("Hello");
+            System.out.println("Hello");
+        }
+    }
+
