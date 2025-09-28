a720a0eb55ab6b9bfbbf5af5a3a537b9f5b7a38f
Reega-Maria <mariareega4@gmail.com>
2025-09-28 19:53:45 +0530

    Type Casting

---
+++ src/ex_05_Type_Casting/Lab058_Type_Casting.java
@@ -0,0 +1,10 @@
+package ex_05_Type_Casting;
+
+public class Lab058_Type_Casting {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b; // Valid Syntax -> Widening -> Implicit Casting - Automatically done.
+        int a1 = (int) b; // Valid Syntax -> Widening -> Explicit Casting - not required
+    }
+}
+
