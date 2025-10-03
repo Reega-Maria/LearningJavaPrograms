9f9f0d435cbebcff15e84f4f779c91e19b17df1d
Reega-Maria <mariareega4@gmail.com>
2025-10-03 20:26:09 +0530

    While Loop

---
+++ src/ex_11_While/Lab116_While_2.java
@@ -0,0 +1,12 @@
+package ex_11_While;
+
+public class Lab116_While_2 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 15) {
+            System.out.println("Reega");
+            i++;
+        }
+    }
+}
+
---
+++ src/ex_11_While/Lab118_While_4.java
@@ -0,0 +1,10 @@
+package ex_11_While;
+
+public class Lab118_While_4 {
+        public static void main(String[] args) {
+            while (true){
+                System.out.println("Hello, Reega");
+            }
+        }
+    }
+//Process finished with exit code 130
