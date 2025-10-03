8a1c8bf1c5bd8a52c6e465fdcce4d52e3033d149
Reega-Maria <mariareega4@gmail.com>
2025-10-03 20:54:44 +0530

    Byte Data type

---
+++ src/ex_01_Java_Basics/Lab023_Byte.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basics;
+
+public class Lab023_Byte {
+        public static void main(String[] args) {
+            byte age = 10;
+
+//        String s = "Pramod";
+//        // p,r,a,m,o,d -> char - Primitive
+//
+
+
+
+        }
+    }
+
+
