2272f50add13e36e4faf201fe26c27b7ff19180e
Reega-Maria <mariareega4@gmail.com>
2025-10-13 23:33:37 +0530

    Check if a Number is Even or Odd.

---
+++ src/Tasks/Even_Odd.java
@@ -0,0 +1,18 @@
+package Tasks;
+//Check if a Number is Even or Odd.
+import java.util.Scanner;
+public class Even_Odd {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is Odd");
+
+        }
+    }
+}
+
+
