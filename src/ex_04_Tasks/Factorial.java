719f220ecabb90c912d7aa2b54a4c763e3a9c7b0
Reega-Maria <mariareega4@gmail.com>
2025-11-07 13:31:06 +0530

    Factorial

---
+++ src/ex_04_Tasks/Factorial.java
@@ -0,0 +1,26 @@
+package ex_04_Tasks;
+import java.util.Scanner;
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = scanner.nextInt();
+        int fact = 1;
+        if (n == 0) {
+            fact = 1;
+        }
+
+        // for (int i = n; i >=1 ; i--) {
+        for (int i = 1; i <= n ; i++) {
+            fact = fact*i;
+        }
+        System.out.println(fact);
+
+
+
+
+
+
+    }
+}
