f7b1582b994f7e304eaf128d6db4ce850e523ced
Reega-Maria <mariareega4@gmail.com>
2025-11-07 13:25:12 +0530

    Non primative Literals

---
+++ src/ex_02_Literals/Lab035_Non_Primative_Literals.java
@@ -0,0 +1,9 @@
+package ex_02_Literals;
+
+public class Lab035_Non_Primative_Literals {
+        public static void main(String[] args) {
+            String name = "Reega"; // String is bunch of char.
+            int[] arrays_of_items = new int[10];
+
+        }
+    }
