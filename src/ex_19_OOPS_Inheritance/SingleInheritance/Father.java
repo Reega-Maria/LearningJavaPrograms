caee3f22c791b2d5d572178d32ce6caaed4f012b
Reega-Maria <mariareega4@gmail.com>
2025-11-16 08:32:06 +0530

    Inheritance

---
+++ src/ex_19_OOPS_Inheritance/SingleInheritance/Father.java
@@ -0,0 +1,9 @@
+package ex_19_OOPS_Inheritance.SingleInheritance;
+
+public class Father {
+    int gold_f = 1000;
+
+    void bhk2() {
+        System.out.println("Father 2 BHK!");
+    }
+}
---
+++ src/ex_19_OOPS_Inheritance/SingleInheritance/Son.java
@@ -0,0 +1,7 @@
+package ex_19_OOPS_Inheritance.SingleInheritance;
+
+public class Son extends Father{
+    void bhk3(){
+        System.out.println("BHK3");
+    }
+}
