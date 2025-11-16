1945aaeaa112348a6aa322795f213b5cf3668ad5
Reega-Maria <mariareega4@gmail.com>
2025-11-16 08:15:34 +0530

    inheritance

---
+++ src/ex_19_OOPS_Inheritance/Hierarchical/Lucky.java
@@ -0,0 +1,7 @@
+package ex_19_OOPS_Inheritance.Hierarchical;
+
+public class Lucky extends Father {
+    void l2(){
+        System.out.println("Lucky");
+    }
+}
---
+++ src/ex_19_OOPS_Inheritance/Hierarchical/Ruhani.java
@@ -0,0 +1,6 @@
+package ex_19_OOPS_Inheritance.Hierarchical;
+public class Ruhani extends Father{
+    void r1(){
+        System.out.println("ruhani");
+    }
+}
